89fb40b8cd1681cf50ed42a8e00b53f49d5268e7
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2023-07-25 23:34:54 -0400

    feat: applying google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/Navbar";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./global.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,6 +23,21 @@ export default function RootLayout({
           <Navbar />
         </div>
         {children}
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script strategy="lazyOnload" id="gtag">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+          `}
+        </Script>
       </body>
     </html>
   );
