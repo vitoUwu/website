d29e242259c1d02aa266985ac9fe1809e38cb831
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2023-09-07 22:14:45 -0400

    chore: rebuild with Astro :D

---
+++ src/lib/constants/jobs.ts
@@ -0,0 +1,25 @@
+const jobs = [
+  {
+    name: "Deco.cx",
+    description:
+      "A deco.cx é uma plataforma moderna de desenvolvimento web que oferece uma stack de tecnologia simples e eficiente combinada com recursos poderosos para criar websites extremamente rápidos. A stack da deco.cx é baseada em Deno, Twind e Fresh. Com essa stack é possível atingir altos pontos de performance, tanto mobile quanto desktop, gerando entre 90 e 100 pontos no Lighthouse.",
+    start: new Date("08/16/2023"),
+    role: "Freelancer",
+  },
+  {
+    name: "Código Kid",
+    start: new Date("08/16/2023"),
+    role: "Instrutor",
+    time: "Meio Período",
+  },
+  {
+    name: "Código Kid",
+    description:
+      "- Instruir e auxiliar alunos de 6 a 12 anos nas aulas na plataforma online.\n- Criar aulas experimentais para que novos alunos possam se familiarizar com o conteúdo.\n- Desenvolver projetos de robótica e de jogos para eventos dentro da escola.",
+    start: new Date("04/29/2023"),
+    end: new Date("08/16/2023"),
+    role: "Instrutor",
+  },
+];
+
+export default jobs;
