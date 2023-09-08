4f34b0501a5632216d0b46ad1081fd837b6db15d
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2023-09-07 23:38:05 -0400

    refactor: SEO, sitemap & GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  })]
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }),
+    sitemap()
+  ],
+  site: "https://vitoo.dev/"
 });
