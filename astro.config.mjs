9e104882b058925ec7794639706f8b39987af7bd
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2023-09-10 14:52:58 -0400

    feat: add prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +10,8 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false
     }),
-    sitemap()
+    sitemap(),
+    prefetch()
   ],
   site: "https://vitoo.dev/"
 });
