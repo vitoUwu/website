8bbb7f381f42f4f70b126ef8ff4ed0b3333c1939
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2023-09-25 20:23:34 -0400

    chore: updating jobs

--- src/lib/constants/jobs.ts
+++ src/lib/constants/jobs.ts
@@ -9,16 +9,18 @@ const jobs = [
   {
     name: "Código Kid",
     start: new Date("08/16/2023"),
+    end: new Date("10/15/2023"),
     role: "Instrutor",
     time: "Meio Período",
   },
   {
     name: "Código Kid",
     description:
-      "- Instruir e auxiliar alunos de 6 a 12 anos nas aulas na plataforma online.\n- Criar aulas experimentais para que novos alunos possam se familiarizar com o conteúdo.\n- Desenvolver projetos de robótica e de jogos para eventos dentro da escola.",
+    "Instruir e auxiliar alunos de 6 a 12 anos nas aulas na plataforma online.\nCriar aulas experimentais para que novos alunos possam se familiarizar com o conteúdo.\nDesenvolver projetos de robótica e de jogos para eventos dentro da escola.",
     start: new Date("04/29/2023"),
     end: new Date("08/16/2023"),
     role: "Instrutor",
+    time: "Tempo Integral",
   },
 ];
 
