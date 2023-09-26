c05d4f4a2e00a25f043aba1a2c5ea5af059154dc
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2023-09-25 20:23:19 -0400

    refactor: trying something....

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import prefetch from "@astrojs/prefetch";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false
-    }),
-    sitemap(),
-    prefetch()
-  ],
-  site: "https://vitoo.dev/"
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap(), prefetch()],
+  site: "https://vitoo.dev/",
+  output: "server",
+  adapter: vercel(),
 });
