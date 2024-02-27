894e62b1ef8391e4c2ddeeabc51198f0b9493516
vitoo <32278696+vitoUwu@users.noreply.github.com>
2024-02-27 09:34:12 -0400

    Update jobs.ts

--- src/lib/constants/jobs.ts
+++ src/lib/constants/jobs.ts
@@ -3,15 +3,15 @@ const jobs = [
     name: "ITZA Tech",
     description:
       "Desenvolvemos e-commerces utilizando a Deco.cx, uma plataforma que oferece uma stack de tecnologia simples e eficiente combinada com recursos poderosos para criar websites extremamente rápidos baseada em Deno, Twind e Fresh,  essas tecnologias proporcionam uma performance capaz de atingir entre 90 e 100 pontos no PageSpeed",
-    start: new Date("08/16/2023"),
+    start: new Date("08/1/2023"),
     role: "Desenvolvedor Front-end",
   },
   {
     name: "Código Kid",
     description:
     "Instruir e auxiliar alunos de 6 a 12 anos nas aulas na plataforma online.\nCriar aulas experimentais para que novos alunos possam se familiarizar com o conteúdo.\nDesenvolver projetos de robótica e de jogos para eventos dentro da escola.",
-    start: new Date("04/29/2023"),
-    end: new Date("10/15/2023"),
+    start: new Date("04/1/2023"),
+    end: new Date("10/1/2023"),
     role: "Instrutor",
   },
 ];
