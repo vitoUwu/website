62d77f2b7cd36b4941aefa72a7ef81483b4116a5
vitoo <32278696+vitoUwu@users.noreply.github.com>
2024-04-06 16:53:44 -0400

    Update jobs.ts

--- src/lib/constants/jobs.ts
+++ src/lib/constants/jobs.ts
@@ -2,7 +2,7 @@ const jobs = [
   {
     name: "ITZA Tech",
     description:
-      "Desenvolvo e-commerce headless utilizando a deco.cx, Fresh, Tailwind, Typescript e Deno, atingindo uma alta performance e navegações mais fluidas. Otimizei sites, melhorei tempos de carregamento e fiz contribuições para a deco.cx. Fui reconhecido pela Deco como contribuidor da semana 7 vezes seguidas",
+      "Desenvolvo e-commerce headless utilizando a deco.cx, Fresh, Tailwind, Typescript e Deno, atingindo uma alta performance e navegações mais fluidas. Otimizei sites, melhorei tempos de carregamento e fiz contribuições para a deco.cx. Fui reconhecido pela Deco como contribuidor da semana 10 vezes seguidas",
     start: new Date("08/1/2023"),
     role: "Desenvolvedor Front-end",
   },
