2309b9ee9e7c38d06db25743616d57572c3ef816
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-04-20 23:21:15 -0400

    feat: add meta tags to 404 page

--- pages/[...catchall].vue
+++ pages/[...catchall].vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+const title = "404 :P";
+const description = "Página não encontrada";
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogImage: "/404_not_found.png",
+  ogUrl: "https://vitoo.dev",
+  twitterTitle: title,
+  twitterDescription: description,
+  twitterImage: "/404_not_found.png",
+  twitterCard: "summary",
+});
+</script>
+
 <template>
   <NotFound text="Página não encontrada" href="/" />
 </template>
