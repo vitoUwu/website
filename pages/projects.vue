e013b4377838ba4965654654d417e444c54b5c1c
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-04-20 23:51:28 -0400

    feat: add language usage

--- pages/projects.vue
+++ pages/projects.vue
@@ -17,7 +17,24 @@ useSeoMeta({
   twitterCard: "summary",
 });
 
-const reposLength = Object.keys(repos).length;
+const reposValues = Object.values(repos);
+const reposLength = reposValues.length;
+
+const languagesPercentage = [
+  ...new Set(reposValues.map((repo) => repo.language ?? "Unknown")),
+]
+  .map((language) => {
+    const amount = reposValues.filter(
+      (repo) =>
+        (language === "Unknown" && !repo.language) || repo.language === language
+    ).length;
+    return {
+      language,
+      amount,
+      percentage: (amount / reposLength) * 100,
+    };
+  })
+  .sort((a, b) => b.amount - a.amount);
 </script>
 
 <template>
@@ -31,6 +48,33 @@ const reposLength = Object.keys(repos).length;
         <h1>{{ reposLength }} Projetos</h1>
       </nav>
     </header>
+    <p class="mb-3">Linguagens mais usadas</p>
+    <ul class="mb-10 select-none space-y-3">
+      <li
+        class="relative"
+        v-for="(language, index) of languagesPercentage.slice(0, 5)"
+        :key="language.language"
+      >
+        <div class="flex items-center gap-3">
+          <span
+            class="shrink-0 text-zinc-400 group-hover:text-zinc-200 transition-all"
+          >
+            <span class="text-emerald-600">{{ index + 1 }}.</span>
+            {{ language.language }}
+          </span>
+          <span
+            class="text-zinc-500 group-hover:text-emerald-600 text-xs transition-all shrink-0"
+          >
+            {{ language.amount }} projetos - ({{
+              language.percentage.toFixed(2)
+            }}%)
+          </span>
+          <span
+            class="border-b border-dashed border-zinc-500 w-full group-hover:border-emerald-600 transition-all"
+          />
+        </div>
+      </li>
+    </ul>
     <div class="space-y-3">
       <a
         v-for="repo of repos"
@@ -45,12 +89,12 @@ const reposLength = Object.keys(repos).length;
         </span>
         <span
           v-if="repo.language"
-          class="text-zinc-600 group-hover:text-emerald-600 text-xs transition-all shrink-0"
+          class="text-zinc-500 group-hover:text-emerald-600 text-xs transition-all shrink-0"
         >
           {{ repo.language }}
         </span>
         <span
-          class="border-b border-dashed border-zinc-600 w-full group-hover:border-emerald-600 transition-all"
+          class="border-b border-dashed border-zinc-500 w-full group-hover:border-emerald-600 transition-all"
         />
       </a>
     </div>
