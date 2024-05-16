c4aa3ab0013ce315c170a976e049ee11be0efd61
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-05-16 18:51:06 -0400

    chore: update works

--- utils/works.ts
+++ utils/works.ts
@@ -1,17 +1,18 @@
 export default [
-  {
-    name: "ITZA Tech",
-    description:
-      "Desenvolvemos e-commerce headless utilizando a deco.cx, Fresh, Tailwind, Typescript e Deno. Nosso foco é construir e implementar soluções da maneira mais fluida e performática possível, para que o usuário possa navegar de forma suave e sem travamentos ou lentidões. Construi, otimizei e refatorei sites e implementações como Zorba, PG Adesivos, Als, Velocita, True Source entre outras. Melhorei tempos de carregamento reduzindo em torno de 2 a 5 segundos de carregamento, reduzi tamanho de bundles, entregando menos Javascript para o browser. Fiz também contribuições open source para a deco.cx.",
-    start: new Date("08/1/2023"),
-    role: "Desenvolvedor Full Stack",
-  },
-  {
-    name: "Código Kid",
-    description:
-      "Instruir e auxiliar alunos de 6 a 12 anos nas aulas na plataforma online.\nCriar aulas experimentais para que novos alunos possam se familiarizar com o conteúdo.\nDesenvolver projetos de robótica e de jogos para eventos dentro da escola.",
-    start: new Date("04/1/2023"),
-    end: new Date("10/1/2023"),
-    role: "Instrutor",
-  },
+	{
+		name: "ITZA Tech",
+		description:
+			"Desenvolvemos e-commerce headless utilizando a deco.cx, Fresh, Tailwind, Typescript e Deno. Nosso foco é construir e implementar soluções da maneira mais fluida e performática possível, para que o usuário possa navegar de forma suave e sem travamentos ou lentidões. Construi, otimizei e refatorei sites e implementações como Zorba, PG Adesivos, Als, Velocita, True Source entre outras. Melhorei tempos de carregamento reduzindo em torno de 2 a 5 segundos de carregamento, reduzi tamanho de bundles, entregando menos Javascript para o browser. Fiz também contribuições open source para a deco.cx.",
+		start: new Date("08/1/2023"),
+		end: new Date("05/1/2024"),
+		role: "Desenvolvedor Full Stack",
+	},
+	{
+		name: "Código Kid",
+		description:
+			"Instruir e auxiliar alunos de 6 a 12 anos nas aulas na plataforma online.\nCriar aulas experimentais para que novos alunos possam se familiarizar com o conteúdo.\nDesenvolver projetos de robótica e de jogos para eventos dentro da escola.",
+		start: new Date("04/1/2023"),
+		end: new Date("10/1/2023"),
+		role: "Instrutor",
+	},
 ];
