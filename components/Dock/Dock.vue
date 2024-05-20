6a72d5cd44b0c33a19bf83e4df138f8a29857c5c
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-05-20 07:37:13 -0400

    fix: only show spotify in home

--- components/Dock/Dock.vue
+++ components/Dock/Dock.vue
@@ -1,8 +1,12 @@
+<script setup lang="ts">
+const { path } = useRoute();
+</script>
+
 <template>
   <div
     class="fixed gap-2 bottom-5 left-3 right-3 sm:left-5 sm:right-5 flex justify-between items-end"
   >
-    <DockSpotify />
+    <DockSpotify v-if="path === '/'" />
     <DockLocales />
   </div>
 </template>
