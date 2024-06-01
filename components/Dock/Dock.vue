bd869115b60ea9d164cb2faaaf9c61e0f67b3fa0
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-06-01 10:19:14 -0400

    fix: remove path trailling slash

--- components/Dock/Dock.vue
+++ components/Dock/Dock.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
-const { path } = useRoute();
+const route = useRoute();
+
+function removeTraillingSlash(path: string) {
+  return path.replace(/\/$/, "");
+}
+
+const path = removeTraillingSlash(route.path);
+const pathsToShowSpotify = ["/", "/en"];
+const showSpotify = pathsToShowSpotify.includes(path);
 </script>
 
 <template>
   <div
     class="fixed gap-2 bottom-5 left-3 right-3 sm:left-5 sm:right-5 flex justify-between items-end"
   >
-    <DockSpotify v-if="path === '/' || path === '/en'" />
+    <DockSpotify v-if="showSpotify" />
     <DockLocales />
   </div>
 </template>
